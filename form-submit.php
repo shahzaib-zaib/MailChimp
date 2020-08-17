d850bed4b4a3a714c0f0d962b834f9822fee524c
Shahzaib <shahzaibux@gmail.com>
2020-08-17 15:47:22 +0500

    Updated Project

--- form-submit.php
+++ form-submit.php
@@ -1,5 +1,13 @@
 <?php
+    include "lib/MailChimp.php";
 
+    use \DrewM\MailChimp\MailChimp;
+
+    $api_key = "";
+    $list_id = "";
+
+    $MailChimp = new MailChimp($api_key);
+    
     if(isset($_POST['submit'])){
         $fname = $_POST['fname'];
         $lname = $_POST['lname'];
@@ -7,10 +15,12 @@
         $companyName = $_POST['comnamy'];
         $employeeno = $_POST['employ'];
 
-        echo $fname . " " . $lname;
-        echo $email;
-        echo $companyName;
-        echo $employeeno;
+        $result = $MailChimp->post("lists/$list_id/members", [
+				'email_address' => 'davy@example.com',
+				'status'        => 'subscribed',
+			]);
+
+        print_r($result);
     }
 
 ?>
